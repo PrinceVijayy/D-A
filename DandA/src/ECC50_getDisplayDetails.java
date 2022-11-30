968705961a90d4a04c21b3d487bb1a543675fd2e
PrinceVijayy <kondavijayy123@gmail.com>
2022-11-30 08:10:25 +0530

    51,52

--- DandA/src/ECC50_getDisplayDetails.java
+++ DandA/src/ECC50_getDisplayDetails.java
@@ -1,17 +1,21 @@
 public class ECC50_getDisplayDetails {
     public static void main(String[] args) {
-        System.out.println(getDisplayDetails(33,3.2f));
+
+        System.out.println(getDisplayDetails(33, 3.2f));
+        System.out.println(getDisplayDetails(130, 20.4f));
     }
-    public static String getDisplayDetails(int runs,float overs){
-        String result="";
-        int balls=0;
-        int oversBeforePoint=(int)overs;
-        int ballsBeforePoint=oversBeforePoint*6;
-        int ballsAfterPoint=(int)(overs*10)-(oversBeforePoint*10);
-        int ballForGivenOvers= ballsBeforePoint+ballsAfterPoint;
-        float runRateInFloatFormat=((float)runs/(float)ballForGivenOvers);
-        String runRateFormated=String.format("%.1f",runRateInFloatFormat);
-        System.out.println(runRateFormated);
-        return result;
+
+    public static String getDisplayDetails(int runs, float overs) {
+        int oversBeforePoint = (int) overs;
+        int ballsBeforePoint = oversBeforePoint * 6;
+        int ballsAfterPoint = (int) (overs * 10) - (oversBeforePoint * 10);
+        int balls = ballsBeforePoint + ballsAfterPoint;
+        float requiredRunRateInFloat = ((float) runs / (float) balls);
+        String requiredRunRatePerBall = String.format("%.2f", requiredRunRateInFloat);
+        String requiredRunRatePerOver = String.format("%.2f", requiredRunRateInFloat * 6);
+        if (runs < 100 || balls < 100)
+            return runs + " runs for " + balls + " balls @ " + requiredRunRatePerBall + "runs per ball";
+        else return runs + " runs for " + overs + " overs @ " + requiredRunRatePerOver + "runs per over";
+
     }
 }
